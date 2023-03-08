60387d01894f05f60c30bf3bced4fb1977299ff4
“Valentina-Bianchini” <bianchinival19@gmail.com>
2023-03-08 20:19:02 -0300

    ay estupida mis estilos idiotaa

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card(props) {
     return (
-        <div>
+        <div class="card">
             <h2>Información del alumno:</h2>
             <p>Nombre: {props.nombre}</p>
             <p>Mail: {props.email}</p>
